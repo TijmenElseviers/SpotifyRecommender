817ad9c4e50cf5b8946efc7096db502bad56fe07
TijmenElseviers <tijmenelseviers@gmail.com>
2020-11-05 12:12:14 +0100

    Added track cleaner improvements: Can now get the full playlist instead of only 100 songs. Added audio analysis function: it is now possible to add audio analysis to the dataframe

--- spotifyrecommender/data/track_cleaner.py
+++ spotifyrecommender/data/track_cleaner.py
@@ -16,16 +16,23 @@ def get_tracks(like):
         if(playlist['like'] == like):
             playlist_id = uri.split(':')[2]
 
-    return sp.user_playlist("", playlist_id, 'tracks')
+    results = sp.user_playlist_tracks("", playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+
+    return tracks
+
 
 def clean_tracks(like):
-    playlist_tracks_data = get_tracks(like)['tracks']
+    playlist_tracks_data = get_tracks(like)
     playlist_tracks_id = []
     playlist_tracks_titles = []
     playlist_tracks_artists = []
     playlist_tracks_first_artists = []
 
-    for track in playlist_tracks_data['items']:
+    for track in playlist_tracks_data:
         playlist_tracks_id.append(track['track']['id'])
         playlist_tracks_titles.append(track['track']['name'])
         artist_list = []
@@ -34,8 +41,42 @@ def clean_tracks(like):
         playlist_tracks_artists.append(artist_list)
         playlist_tracks_first_artists.append(artist_list[0])
 
-    features = sp.audio_features(playlist_tracks_id)
-    return pd.DataFrame(data=features, columns=features[0].keys())
+
+    chunks = [playlist_tracks_id[x:x+100] for x in range (0, len(playlist_tracks_id), 100)]
+
+    features = []
+    for chunk in chunks:
+            features.extend(sp.audio_features(chunk))
+
+    features_df = pd.DataFrame(data=features, columns=features[0].keys())
+
+    features_df['title'] = playlist_tracks_titles
+    features_df['first_artist'] = playlist_tracks_first_artists
+    features_df['all_artists'] = playlist_tracks_artists
+    features_df = features_df[['id', 'title', 'first_artist', 'all_artists',
+                            'danceability', 'energy', 'key', 'loudness',
+                            'mode', 'acousticness', 'instrumentalness',
+                            'liveness', 'valence', 'tempo',
+                            'duration_ms', 'time_signature']]
+
+    return features_df
+
+def get_audio_analysis(dataframe):
+    num_bars = []
+    num_sections = []
+    num_segments = []
+
+    for i in range(0, len(dataframe['id'])):
+        analysis = sp.audio_analysis(dataframe.iloc[i]['id'])
+        num_bars.append(len(analysis['bars']))
+        num_sections.append(len(analysis['sections']))
+        num_segments.append(len(analysis['segments']))
+
+    dataframe['num_bars'] = num_bars
+    dataframe['num_sections'] = num_sections
+    dataframe['num_segments'] = num_segments
+
+    return dataframe
 
 def clean_console():
     print("\nNo cleaning data to CSV has been implemented yet!")
--- spotifyrecommender/tests/test_data.py
+++ spotifyrecommender/tests/test_data.py
@@ -1,22 +1,48 @@
 from data.track_cleaner import clean_tracks
+from data.track_cleaner import get_audio_analysis
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def check_dataframe_like():
-    liked = clean_tracks(True)
-    print("--------------------")
-    print("Liked Cleaned Tracks")
-    print("--------------------")
-    print(liked.tail())
+def check_dataframe(liked):
+    df = clean_tracks(liked)
+    if(liked):
+        print("--------------------")
+        print("Liked Cleaned Tracks")
+        print("--------------------")
+    else:
+        print("------------------------")
+        print("Not Liked Cleaned Tracks")
+        print("------------------------")
+    
+    print(df.tail())
+    print("------------------------")
+    print("Shape: {}".format(len(df)))
+    print("------------------------")
 
-def check_dataframe_nolike():
-    notLiked = clean_tracks(False)
-    print("------------------------")
-    print("Not Liked Cleaned Tracks")
-    print("------------------------")
-    print(notLiked.tail())
+
+def plot_basic_figures(liked):
+    df = clean_tracks(liked)
+    if(liked):
+        print("-------------")
+        print("Liked Artists")
+        print("-------------")
+    else:
+        print("-----------------")
+        print("Not Liked Artists")
+        print("-----------------")
+
+    plt.figure(figsize=(20,30))
+    sns.countplot(df['first_artist'])
+    plt.xticks(rotation=90)
+    plt.show(block=True)
+
+
 
 def console_tester():
     print("\nAll tests will be executed:\n")
 
-    check_dataframe_like()
-    check_dataframe_nolike()
+    check_dataframe(False)
+    check_dataframe(True)
+    plot_basic_figures(False)
+    plot_basic_figures(True)
     
